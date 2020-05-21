fc92817294ce644525f0c7e4ebcb6d6a20d8e94a
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-21 03:03:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   array.collect { |x| x**2}
-  output = []
-  array.each do |item|
-    output << item**2
-  end
-  return output
+  # output = []
+  # array.each do |item|
+  #   output << item**2
+  # end
+  # return output
 end
 
 array = [1, 2, 3]
